b58f861c07e7999783f20f4248ad21ae22c11216
pangge <939136265@qq.com>
2019-10-14 01:11:01 +0800

    new

---
+++ whale/operation/operator.cpp
@@ -0,0 +1,8 @@
+#include <cstring>
+#include "operation/operator.h"
+
+namespace whale {
+
+
+
+}
--- whale/operation/operator.h
+++ whale/operation/operator.h
@@ -8,13 +8,11 @@
 namespace whale {
 
 struct OpParam {
-    const char* name = "Unknown"; ///< op name
-    std::string doc;              ///< operator doc.
+    const char* name = "Unknown"; ///< parameter name
+    std::string doc;              ///< parameter doc.
 
     bool is_commutative{true};    ///< true default.
                                   ///< Judge if the operation is commutative.
-    ///< Operator paremeter map:  parameter name ---> arguments.
-    ///std::unordered_map<std::string, Argument> Args_map;
     unsigned char buffer[0];
 };
 
@@ -24,7 +22,10 @@ public:
     ~Op();
 
     template<typename T>
-    Op& set(const char* name, T default_val) {
+    Op& set(const char* name, const T& default_val) {
+        OpParam* param_ptr = new OpParam();
+        param_ptr->name = name;
+        this->append(param_ptr, &default_val, sizeof(T));
     }
 
     template<typename T>
@@ -35,8 +36,10 @@ public:
 
 private:  
 
+    void append(OpParam* param_ptr, const char* data, size_t bytes);
+
 private:
-    std::unordered_map<std::string, size_t> _args_map; 
+    std::unordered_map<std::string, OpParam*> _args_map; 
     OpParam _param;
 };
 
